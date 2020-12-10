53ff1a98ee30b1e6ebe9de8981a7e9dc0d51cee1
jessicacamara <jessi_camara@hotmail.com>
2020-12-10 23:54:27 +0000

    Add RestController

--- captainDrink/src/main/java/org/academiadecodigo/gnunas/captaindrink/GameController.java
+++ captainDrink/src/main/java/org/academiadecodigo/gnunas/captaindrink/GameController.java
@@ -1,43 +1,60 @@
 package org.academiadecodigo.gnunas.captaindrink;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.support.ServletContextResource;
+
+import javax.servlet.ServletContext;
+import java.util.List;
 
 @CrossOrigin(origins = "*")
 @RestController
+@
 public class GameController {
 
     private Game game;
+    private ServletContext servletContext;
 
     @Autowired
     public GameController(Game game) {
         this.game = game;
     }
 
-    @RequestMapping(method = RequestMethod.GET, path = {"", "/"})
-    public String getHomePage(Model model) {
-
-        return "homePage";
+    @Autowired
+    public void setServletContext(ServletContext servletContext) {
+        this.servletContext = servletContext;
     }
 
-    @RequestMapping(method = RequestMethod.GET, path = "newPlayer")
-    public String getPlayerForm(Model model) {
-
-        Player player = new Player();
-        model.addAttribute("player", player);
-
-        return "playerForm";
-    }
-
-    @RequestMapping(method = RequestMethod.POST, path = "newPlayer")
-    public String newPlayer(@ModelAttribute("player") Player player) {
+    @RequestMapping(method = RequestMethod.POST, path = "")
+    public ResponseEntity<?> newPlayer(@RequestBody Player player) {
 
         game.addPlayer(player);
 
-
-
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
+
+    @GetMapping("")
+    public ResponseEntity<List<Player>> listAllPlayers() {
+
+        return new ResponseEntity<>(game.getAllPlayers(), HttpStatus.OK);
+    }
+
+    @GetMapping("/card")
+    public Resource getCardImage() {
+
+        String path = game.getRandomCard().getImgPath();
+        return new ServletContextResource(servletContext, "/resources/" + path);
+    }
+
+    @GetMapping("/player")
+    public ResponseEntity<Player> getRandomPlayer() {
+
+        return new ResponseEntity<>(game.randomPlayer(), HttpStatus.OK);
+    }
+
+
 
 }
