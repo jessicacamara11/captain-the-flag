6d249090a6b8f5763cb26f1b5c814b7ff8d9f18b
miguelteixas <miguelrteixas@gmail.com>
2020-12-11 03:24:31 +0000

    add rules to each card

--- captainDrink/src/main/java/org/academiadecodigo/gnunas/captaindrink/Card.java
+++ captainDrink/src/main/java/org/academiadecodigo/gnunas/captaindrink/Card.java
@@ -41,19 +41,45 @@ public class Card {
     }
     public enum Rank {
 
-        ACE("You have to do bad stuff", "ace"),
-        TWO("You have to do bad stuff", "two"),
-        THREE("You have to do bad stuff", "three"),
-        FOUR("You have to do bad stuff", "four"),
-        FIVE("You have to do bad stuff", "five"),
-        SIX("You have to do bad stuff", "six"),
-        SEVEN("You have to do bad stuff", "seven"),
-        EIGHT("You have to do bad stuff", "eight"),
-        NINE("You have to do bad stuff", "nine"),
-        TEN("You have to do bad stuff", "ten"),
-        QUEEN("You have to do bad stuff", "queen"),
-        JACK("You have to do bad stuff", "jack"),
-        KING("You have to do bad stuff", "king");
+        ACE("Paranoia \n " +
+                "The person on your right whispers you a question which the answer must be somebody playing the game" +
+                "(e.g. Who is the hottest in the room?).\n" +
+                "Your must respond out loud.\n" +
+                "If someone wants to know what was the question, they have to drink.", "ace"),
+        TWO("You drink.", "two"),
+        THREE("Choose another player to drink", "three"),
+        FOUR("Fuzzy Duck\n" +
+                "Take turns saying the words \"fuzzy duck\".\n" +
+                "A player may also opt to say \"does he?\", in which case play resumes in the opposite direction with players saying \"ducky fuzz\" instead.\n" +
+                "If a player says the wrong thing, plays out of turn or breaks the rhythm of the game they must drink.", "four"),
+        FIVE("Ping Pong\n" +
+                "Take turns counting from 1.\n" +
+                "When a number is multiple of 5 say \"Ping\" instead.\n" +
+                "If it is a multiple of 7 say \"Pong\".\n" +
+                "In case the number is a palindrome you must say \"Ping Pong\".\n" +
+                "The first person to fail must drink.", "five"),
+        SIX("Never Have I Ever\n" +
+                "Say a statement about something that you've never done starting with \"Never have i ever...\"\n" +
+                "Anyone who at some point in their life has done that, must drink.", "six"),
+        SEVEN("Tubthumping\n" +
+                "Listen to Tubthumping by Chumbawamba. Everyone drinks everytime you hear the word \"down\".", "seven"),
+        EIGHT("Pick a mate\n" +
+                "Pick someone to drink with you for the rest of the game.", "eight"),
+        NINE("Pennies\n" +
+                "Bounce a coin off the table into a short glass.\n" +
+                "Miss? Drink. 3 in a row? Make a rule.", "nine"),
+        TEN("Most Likely\n" +
+                "Ask a most likely question. (e.g. Who would be most likely to fart in front of strangers?)\n" +
+                "After counting to three everyone points at a person who they think are most likely to do so.\n" +
+                "Who has more fingers pointed at them drinks as many sips.", "ten"),
+        QUEEN("Medusa\n" +
+                "Everyone looks down at the ground. At the count of three everyone looks up and fixes their gaze at another person in the room.\n" +
+                "If you happen to lock eyes with someone you drink and are out.\n" +
+                "Repeat until there are only 2 players left.", "queen"),
+        JACK("Would you rather\n" +
+                "Play a round of would you rather\n" +
+                "The players who choose the least chosen of the two answers must drink.", "jack"),
+        KING("Make a rule.", "king");
 
         private String action;
         private String path;
